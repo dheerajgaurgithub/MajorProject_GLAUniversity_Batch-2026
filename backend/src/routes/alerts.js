44b651540a2e189f1438214379ad849e169f8ad1
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2026-04-17 11:58:23 +0530

    delete

--- backend/src/routes/alerts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getAlerts, sendAlert } = require('../controllers/alertController')
-const { authenticate, isAdmin } = require('../middleware/auth')
-
-router.get('/', authenticate, isAdmin, getAlerts)
-router.post('/send', authenticate, isAdmin, sendAlert)
-
-module.exports = router
--- components/theme-provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { ThemeProvider as CustomThemeProvider } from '@/lib/theme-provider'
-
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <CustomThemeProvider>{children}</CustomThemeProvider>
-}
